57df4ebe84b50bf93f03ce243fe7f676235d1d37
abubakkar32 <abu35-1994@diu.edu.bd>
2020-07-01 11:34:48 +0600

    no message

--- Web Scrapy/Internet Accsess.py
+++ Web Scrapy/Internet Accsess.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 r = requests.get('https://www.cia.gov/library/publications/the-world-factbook/fields/204.html#AF')
 c = r.content
-soup = BeautifulSoup(c, parser='html_parser')
+soup = BeautifulSoup(c, 'html.parser')
 data = soup.findAll('tr')[1:]
 
 data_dictionary = []
@@ -17,3 +20,9 @@ for country in data:
     except:
         pass
 print(data_dictionary)
+
+internet_stats = pd.DataFrame.from_dict(data_dictionary)
+dataMarge = internet_stats.iloc[0:10, 0:]
+
+sns.pairplot(dataMarge, x_vars=dataMarge.columns[0], y_vars=dataMarge.columns[1])
+plt.show()
