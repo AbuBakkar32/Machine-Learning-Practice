98ba772951c1178904a004e16585ffcdad16f25c
abubakkar32 <abu35-1994@diu.edu.bd>
2020-07-08 13:13:11 +0600

    ANN

---
+++ Machine_Learning/ML For Researcher (Udemy)/ANN/Practice ANN(1).py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras import Sequential
+from keras.layers import Dense
+
+df = pd.read_csv('PIMA_Indians.csv')
+df = df.drop('index', axis=1)
+print(df.head())
+
+X = df.iloc[:, :8].head()
+y = df.iloc[:, -1].head()
+
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.evaluate(X, y)
