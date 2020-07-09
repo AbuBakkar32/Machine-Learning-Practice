2d394a9414affb59f12c5802a898e9108f7c3b22
abubakkar32 <abu35-1994@diu.edu.bd>
2020-07-09 19:53:28 +0600

    no message

---
+++ Python Basic/Namata.py
@@ -0,0 +1,18 @@
+num = input("How many Namata you want?\n")
+num = (int(num) + 1)
+for i in range(int(num)):
+    if i == 0:
+        print('*************************')
+        continue
+    for j in range(11):
+        if j == 0:
+            continue
+        result = i * j
+        print(f"{i}x{j}={result}")
+    print('*************************')
+
+name = 'HELLO'
+names = 'RAKIB'
+print(*name, sep='\t')
+print(*names, sep='\t')
+print('*************************')
