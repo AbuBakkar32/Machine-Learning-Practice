9c7c31ac2e7a01445c63ad7226d7a46f4723d7a2
abubakkar32 <abu35-1994@diu.edu.bd>
2020-08-02 19:11:35 +0600

    no message

---
+++ Machine Learning Part-2/Clustering/hc.py
@@ -0,0 +1,23 @@
+# Hierarchical Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
--- json/Json Project (2)/123.dictionary.py
+++ json/Json Project (2)/123.dictionary.py
@@ -12,16 +12,16 @@ def translate(word):
     elif word.upper() in data:
         return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
-        print("did you mean %s instead" % get_close_matches(word, data.keys())[0])
+        print("Did you mean %s instead" % get_close_matches(word, data.keys())[0])
         decide = input("press y for yes or n for no")
         if decide == "y":
             return data[get_close_matches(word, data.keys())[0]]
         elif decide == "n":
-            return ("Your steps on wrong keys")
+            return ("Your have Type on wrong keys")
         else:
             return ("You have entered wrong input please enter just y or n")
     else:
-        print("Your steps on wrong keys")
+        print("Your have Type on wrong keys")
 
 
 word = input("Enter the word you want to search\n")
