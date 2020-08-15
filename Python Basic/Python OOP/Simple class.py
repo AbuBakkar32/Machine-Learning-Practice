94b1c2c16a302b8d94c97924967fb8995f566fb2
abubakkar32 <abu35-1994@diu.edu.bd>
2020-08-15 19:27:23 +0600

    no message

---
+++ Python Basic/Python OOP/Simple class.py
@@ -0,0 +1,17 @@
+class Mobile:
+    old_phone = 'Keypad'
+    new_phone = 'Touch Screen'
+
+    def old_nokia(self):
+        print(self.old_phone)
+
+    def new_iphone(self):
+        print(self.new_phone)
+        
+def main():
+    myMobile = Mobile()
+    myMobile.new_iphone()
+    myMobile.old_nokia()
+
+if __name__ == '__main__':
+    main()
