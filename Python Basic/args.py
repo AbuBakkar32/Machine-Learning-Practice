78914ca89b4bc9235c290a4d508f423327b2f926
abubakkar32 <abu35-1994@diu.edu.bd>
2020-11-21 12:07:45 +0600

    no message

--- Python Basic/args.py
+++ Python Basic/args.py
@@ -3,9 +3,20 @@ def addition(a, b, *args, option=True):
     if option:
         for i in args:
             result += i
-        return a+b+result
+        return a + b + result
     else:
         return result
 
 
-print(addition(9, 10, 5, 6, 10, 10, 100))
+# print(addition(9, 10, 5, 6, 10, 10, 100))
+
+# Kwargs Example
+
+def arg_printer(a, b, option=True, **kwargs):
+    print(a, b)
+    print(option)
+    print(kwargs)
+
+
+print(arg_printer(3, 4, param1=5, param2=6))
+
