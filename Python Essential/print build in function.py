f0a4e1cbbbeae72fec7f658bae28fcb870a10427
abubakkar32 <abu35-1994@diu.edu.bd>
2021-01-04 12:08:25 +0600

    Add time sleep

--- Python Essential/print build in function.py
+++ Python Essential/print build in function.py
@@ -1,9 +1,10 @@
 # How to print all Built-In functions in Python?
 import re
-
+import time
 list = []
 for j in dir(__builtins__):
     if not re.match(r'^[A-Z_]', j):
         list.append(j)
     for a, b, c in zip(list[::3], list[1::3], list[2::3]):
+        time.sleep(2)
         print('{:<20}{:<20}{:<}'.format(a, b, c))
