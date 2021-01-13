96663aa9e41d5ab6eb1432be63419ef2c1dbde62
abubakkar32 <abu35-1994@diu.edu.bd>
2021-01-13 13:22:12 +0600

    no message

--- Python Tricks and tips/Download youtube video.py
+++ Python Tricks and tips/Download youtube video.py
@@ -1,7 +1,7 @@
 import youtube_dl
 
-link = input("Paste your copy youtube downloadable link")
+# link = input("Paste your copy youtube downloadable link \n")
+link = input("Paste your copy youtube downloadable link \n")
 
 ydl = youtube_dl.YoutubeDL()
-ydl.download(link)
-print("♦♦♦Thank for using this App♦♦♦")
+ydl.download([link])
