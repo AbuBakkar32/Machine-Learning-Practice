df604210630ba631a0b5d9b90e73ff9791516411
abubakkar32 <abu35-1994@diu.edu.bd>
2021-01-13 13:15:13 +0600

    no message

---
+++ Python Tricks and tips/Browser_control.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+url = "https://www.amazon.com/iOttie-Dashboard-Windshield-Samsung-Smartphone/dp/B076B27WP6/?_encoding=UTF8&pd_rd_w=nxqA4&pf_rd_p=27643bbf-38ff-4db5-8032-83558fc1281c&pf_rd_r=S6H1D08SWKTKTDZWGMD0&pd_rd_r=64e89477-686f-4970-8c83-c6c59befb944&pd_rd_wg=VfdGE&ref_=pd_gw_unk"
+webbrowser.open_new_tab(url)
--- Python Tricks and tips/Download youtube video.py
+++ Python Tricks and tips/Download youtube video.py
@@ -1,6 +1,7 @@
 import youtube_dl
 
-link = ["https://www.youtube.com/watch?v=hXM1jzaXQOs"]
+link = input("Paste your copy youtube downloadable link")
 
 ydl = youtube_dl.YoutubeDL()
 ydl.download(link)
+print("♦♦♦Thank for using this App♦♦♦")
