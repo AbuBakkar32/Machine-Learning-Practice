96039545841e04adaa096b9140e63a092dc75289
abubakkar32 <abu35-1994@diu.edu.bd>
2021-01-25 17:48:43 +0600

    no message

--- Python Essential/Essential Python Tricks/listtocommaseparated.py
+++ Python Essential/Essential Python Tricks/listtocommaseparated.py
@@ -2,16 +2,14 @@
 
 items = ['foo', 'bar', 'xyz']
 
-print (','.join(items))
+print(','.join(items))
 
 """list of numbers to comma separated"""
 numbers = [2, 3, 5, 10]
 
-print (','.join(map(str, numbers)))
+print(','.join(map(str, numbers)))
 
 """list of mix  data"""
 data = [2, 'hello', 3, 3.4]
 
-print (','.join(map(str, data)))
-
-
+print(','.join(map(str, data)))
