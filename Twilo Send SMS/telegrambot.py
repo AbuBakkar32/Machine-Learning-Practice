2eef2d21d1ed567efe467931aa1992916db0092f
sajidhasan15 <sajid.22.14.15@gmail.com>
2022-06-09 18:39:32 +0600

    09-06-2022

--- Twilo Send SMS/telegrambot.py
+++ Twilo Send SMS/telegrambot.py
@@ -1,6 +1,6 @@
 # importing all required libraries
-import telebot
-from telethon.sync import TelegramClient
+#import telebot
+#from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerUser, InputPeerChannel
 from telethon import TelegramClient, sync, events
 
