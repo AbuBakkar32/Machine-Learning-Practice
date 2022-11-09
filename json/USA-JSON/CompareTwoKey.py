a3b026f452d79efa404b44cff32b8e40d29b9620
abubakkar32 <abu35-1994@diu.edu.bd>
2022-11-09 23:23:50 +0600

    add file

--- json/USA-JSON/CompareTwoKey.py
+++ json/USA-JSON/CompareTwoKey.py
@@ -1,4 +1,5 @@
 from termcolor import colored
+
 """
 Here are some assumptions to consider:
 * All of the data will be presented as strings.  Do not attempt to interpret the data any further (integers, floats, Booleans, etc.).
@@ -7,8 +8,8 @@ Here are some assumptions to consider:
 """
 
 
-# It takes two dictionaries and converts them into a single dictionary.
-class ConvertTwoDict:
+# It takes two dictionaries and converts them into a single dictionary to Compare tow Dict.
+class CompareTwoDict:
     # Initialize the class
     def __init__(self, dict1, dict2):
         """
@@ -20,19 +21,17 @@ class ConvertTwoDict:
         self.dict1 = dict1
         self.dict2 = dict2
 
-    def converDatatypeToString(self, data):
+    def convertDataTypeToString(self, data):
         """
         It takes a dictionary as an argument and returns a dictionary with all the values converted to strings
 
         :param data: The data to be converted
         :return: a dictionary with the key and value being the same.
         """
-        for k, v in data.items():
-            try:
-                if type(v) == int or type(v) == float or type(v) == bool:
-                    data = {k.translate({32: None}): str(v) for k, v in data.items()}
-            except ValueError:
-                raise ValueError("Value is not a number or integer or boolean")
+        try:
+            data = {k.translate({32: None}): str(v) for k, v in data.items()}
+        except ValueError:
+            raise ValueError("Value is not a number or integer or boolean")
         return data
 
     def outputDict(self, data):
@@ -52,39 +51,52 @@ class ConvertTwoDict:
         """
         This function takes two dictionaries as input and compares the keys and values of the two dictionaries.
 
-        If the key and value of the first dictionary is present in the second dictionary, then it prints the key and value
-        in a table format
+        If the key and value of the first dictionary is present in the second dictionary, then it will return a list of key
 
         :param dict1: The expected dictionary
         :param dict2: The dictionary that is returned from the function
         """
         # call the outputDict method
-        print(colored("'Expected' Message:", 'green', attrs=['reverse', 'blink', 'bold']))
+        finalDict = []
+        for key, value in dict1.items():
+            if dict1[key] not in dict2[key]:
+                finalDict.append(key)
+        return finalDict
+
+    def result(self, dict1, dict2):
+        """
+        It compares two dictionaries and prints the output in a table format
+
+        :param dict1: The expected output
+        :param dict2: The actual/returned message
+        """
+        print(colored("'Expected' Message:", attrs=['blink', 'bold']))
         self.outputDict(dict1)
-        print(colored("\n\n\n'Actual/Returned' Message:", "green", attrs=['reverse', 'blink', 'bold']))
+        print(colored("\n\n\n'Actual/Returned' Message:", attrs=['blink', 'bold']))
         self.outputDict(dict2)
-        print(colored("\n\n\n'Output Message whose values do not match:", "green", attrs=['reverse', 'blink', 'bold']))
+        print(colored("\n\n\n'Output Message whose values do not match:", attrs=['blink', 'bold']))
         # Printing the output in a table format.
         print("+-------------------------+---------------------------------------------------------------------------+")
         print("|Key                      |            Expected Value            |            Actual Value            |")
         print("+-------------------------+---------------------------------------------------------------------------+")
-        for key, value in dict1.items():
-            if dict1[key] not in dict2[key]:
-                print(f"|{key:25}|{dict1[key]:^38}|{dict2[key]:^36}|")
+        compareKey = self.compareTwoKey(dict1, dict2)
+        for key in compareKey:
+            print(f"|{key:25}|{dict1[key]:^38}|{dict2[key]:^36}|")
         print("+-------------------------+---------------------------------------------------------------------------+")
 
-    def comparetTwoDict(self):
+    def compareDictonary(self):
         """
         It compares two dictionaries and returns the difference between them.
-        :return: the output of the compareTwoKey function.
+        :return: the output of the result function.
         """
-        output_dict1 = self.converDatatypeToString(self.dict1)
-        output_dict2 = self.converDatatypeToString(self.dict2)
-        return self.compareTwoKey(output_dict1, output_dict2)
+        output_dict1 = self.convertDataTypeToString(self.dict1)
+        output_dict2 = self.convertDataTypeToString(self.dict2)
+        return self.result(output_dict1, output_dict2)
 
 
 # This is the main function. It is executed when the program is run.
 if __name__ == "__main__":
+    # Creating two dictionaries with dammy data
     data1 = {
         "account": "BATS",
         "clordid": None,
@@ -123,5 +135,5 @@ if __name__ == "__main__":
         "targetsubid": '0001',
         "transacttime": None
     }
-    obj = ConvertTwoDict(data1, data2)
-    obj.comparetTwoDict()
+    obj = CompareTwoDict(data1, data2)
+    obj.compareDictonary()
