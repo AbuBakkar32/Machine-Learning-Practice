6394c6ccb0417acd90ff327f6935ec9dead9d7c2
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-02-06 15:14:34 +0600

    NumberToWordConvert.py file added

--- Python Basic/NumberToWordConvert.py
+++ Python Basic/NumberToWordConvert.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def number_to_words(n):
     ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
             "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
@@ -15,3 +18,22 @@ def number_to_words(n):
         return number_to_words(n // 1000000) + "million" + (number_to_words(n % 1000000) if (n % 1000000 != 0) else "")
     return number_to_words(n // 1000000000) + "billion" + (
         number_to_words(n % 1000000000) if (n % 1000000000 != 0) else "")
+
+
+class TestNumberToWords(unittest.TestCase):
+    def test_number_to_words(self):
+        self.assertEqual(number_to_words(1), "one")
+        self.assertEqual(number_to_words(10), "ten")
+        self.assertEqual(number_to_words(21), "twentyone")
+        self.assertEqual(number_to_words(100), "onehundred")
+        self.assertEqual(number_to_words(105), "onehundredandfive")
+        self.assertEqual(number_to_words(1000), "onethousand")
+        self.assertEqual(number_to_words(1005), "onethousandfive")
+        self.assertEqual(number_to_words(1000000), "onemillion")
+        self.assertEqual(number_to_words(1005000), "onemillionfivethousand")
+        self.assertEqual(number_to_words(1000000000), "onebillion")
+        self.assertEqual(number_to_words(1050000000), "onebillionfiftymillion")
+
+
+if __name__ == '__main__':
+    unittest.main()
