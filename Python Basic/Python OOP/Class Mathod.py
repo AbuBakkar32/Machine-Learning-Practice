d3aa5dcc6c620df594c9ffa0cefcb07d90f59745
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-04-28 08:41:41 +0600

    modify code

--- Python Basic/Python OOP/Class Mathod.py
+++ Python Basic/Python OOP/Class Mathod.py
@@ -1,3 +1,5 @@
+
+# Create an Animal Class
 class Animal:
     def __init__(self, **kwargs):
         self._type = kwargs['type'] if 'type' in kwargs else 'Kitten'
@@ -5,15 +7,18 @@ class Animal:
         self._sound = kwargs['sound'] if 'sound' in kwargs else 'Meow'
 
     def type(self, t=None):
-        if t: self._type = t
+        if t:
+            self._type = t
         return self._type
 
     def name(self, n=None):
-        if n: self._name = n
+        if n:
+            self._name = n
         return self._name
 
     def sound(self, s=None):
-        if s: self._sound = s
+        if s:
+            self._sound = s
         return self._sound
 
     def __str__(self):
