20f425a915cfddb8abeb937d679c5a00c556c41b
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-05-01 10:12:50 +0600

    modify code

--- DataCamp Certification Course/MatPlot Practice/Plot 1.py
+++ DataCamp Certification Course/MatPlot Practice/Plot 1.py
@@ -41,25 +41,25 @@ plt.show()
 # -------------------------------------------- stackplot plot show -------------------------------------------
 
 
-# days = [1, 2, 3, 4, 5]
-#
-# sleeping = [7, 8, 6, 11, 7]
-# eating = [2, 3, 4, 3, 2]
-# working = [7, 8, 7, 2, 2]
-# playing = [8, 5, 7, 8, 13]
+days = [1, 2, 3, 4, 5]
+
+sleeping = [7, 8, 6, 11, 7]
+eating = [2, 3, 4, 3, 2]
+working = [7, 8, 7, 2, 2]
+playing = [8, 5, 7, 8, 13]
 #
 # plt.plot([], [], color='m', label='Sleeping', linewidth=5)
 # plt.plot([], [], color='c', label='Eating', linewidth=5)
 # plt.plot([], [], color='r', label='Working', linewidth=5)
 # plt.plot([], [], color='k', label='Playing', linewidth=5)
 #
-# plt.stackplot(days, sleeping, eating, working, playing, colors=['m', 'c', 'r', 'k'])
-#
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('Interesting Graph\nCheck it out')
-# plt.legend()
-# plt.show()
+plt.stackplot(days, sleeping, eating, working, playing, colors=['m', 'c', 'r', 'k'])
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
 
 # -------------------------------------------- Pie plot show -------------------------------------------
 
