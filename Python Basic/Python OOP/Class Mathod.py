2f9915999cbc119c7f854eab48d41b492c3c7cc8
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-05-09 23:06:00 +0600

    new app install

--- Python Basic/Python OOP/Class Mathod.py
+++ Python Basic/Python OOP/Class Mathod.py
@@ -1,34 +1,67 @@
+#
+# # Create an Animal Class
+# class Animal:
+#     def __init__(self, **kwargs):
+#         self._type = kwargs['type'] if 'type' in kwargs else 'Kitten'
+#         self._name = kwargs['name'] if 'name' in kwargs else 'Fluffy'
+#         self._sound = kwargs['sound'] if 'sound' in kwargs else 'Meow'
+#
+#     def type(self, t=None):
+#         if t:
+#             self._type = t
+#         return self._type
+#
+#     def name(self, n=None):
+#         if n:
+#             self._name = n
+#         return self._name
+#
+#     def sound(self, s=None):
+#         if s:
+#             self._sound = s
+#         return self._sound
+#
+#     def __str__(self):
+#         return f'The {self.type()} is named "{self.name()}" and says "{self.sound()}"'
+#
+#
+# def main():
+#     a1 = Animal(type='Kitten', name='Fluffy', sound='Meow')
+#     print(a1)
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-# Create an Animal Class
-class Animal:
-    def __init__(self, **kwargs):
-        self._type = kwargs['type'] if 'type' in kwargs else 'Kitten'
-        self._name = kwargs['name'] if 'name' in kwargs else 'Fluffy'
-        self._sound = kwargs['sound'] if 'sound' in kwargs else 'Meow'
+n, k = map(int, input().split())
+m = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-    def type(self, t=None):
-        if t:
-            self._type = t
-        return self._type
+c = [0] * (max(m + b) + 1)
+d = [0] * (max(m + b) + 1)
 
-    def name(self, n=None):
-        if n:
-            self._name = n
-        return self._name
+a1 = 0
+a2 = 0
 
-    def sound(self, s=None):
-        if s:
-            self._sound = s
-        return self._sound
+for i in range(n):
+    if m[i] > k:
+        continue
+    c[m[i]] += 1
+    a1 += d[k - m[i]]
 
-    def __str__(self):
-        return f'The {self.type()} is named "{self.name()}" and says "{self.sound()}"'
+for i in range(n):
+    if b[i] > k:
+        continue
+    d[b[i]] += 1
+    a2 += c[k - b[i]]
 
+if a1 > a2:
+    print("Mahmoud".capitalize())
+elif a1 == a2:
+    print("Draw".capitalize())
+else:
+    print("Bashar".capitalize())
 
-def main():
-    a1 = Animal(type='Kitten', name='Fluffy', sound='Meow')
-    print(a1)
-
-
-if __name__ == '__main__':
-    main()
+# 7 3
+# 1 1 2 3 4 1 2
+# 2 2 2 1 1 1 2
