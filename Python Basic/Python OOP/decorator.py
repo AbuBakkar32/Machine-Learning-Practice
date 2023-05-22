0f89465526041e0535c0c7b92fdfdd0649658a8c
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-05-22 19:44:16 +0600

    view updated

--- Python Basic/Python OOP/decorator.py
+++ Python Basic/Python OOP/decorator.py
@@ -1,24 +1,45 @@
-def my_decorator(func):
-    def wrapper(*args, **kwargs):
-        # Add your additional functionality here
-        print("Before function execution")
-        result = func(*args, **kwargs)
-        print("After function execution")
-        return result
+# def my_decorator(func):
+#     def wrapper(*args, **kwargs):
+#         # Add your additional functionality here
+#         print("Before function execution")
+#         result = func(*args, **kwargs)
+#         print("After function execution")
+#         return result
+#
+#     return wrapper
+#
+#
+# @my_decorator
+# def my_function():
+#     print("Inside my_function")
+
+
+from datetime import datetime
+
+
+def log_datetime(func):
+    """Log the date and time of a function"""
+
+    def wrapper():
+        func()
+        print(f'Function: {func.__name__}\nRun on: {datetime.today().strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{"-" * 30}')
 
     return wrapper
 
 
-@my_decorator
-def my_function():
-    print("Inside my_function")
+@log_datetime
+def daily_backup():
+    for i in range(10):
+        print(i, end=' ')
+    print("\n")
+    print('Daily backup job has finished.')
+    print("\n")
 
 
-# @my_decorator
-# def function1():
-#     print("Inside function1")
-#
-#
-# @my_decorator
-# def function2():
-#     print("Inside function2")
+def main():
+    daily_backup()
+
+
+if __name__ == '__main__':
+    main()
