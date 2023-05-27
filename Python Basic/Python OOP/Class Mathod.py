64b9c2fcd58efbf53de64af3ebafd98e530d43b9
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-05-27 09:42:16 +0600

    script updated

--- Python Basic/Python OOP/Class Mathod.py
+++ Python Basic/Python OOP/Class Mathod.py
@@ -33,34 +33,34 @@ def main():
 if __name__ == '__main__':
     main()
 
-# n, k = map(int, input().split())
-# m = list(map(int, input().split()))
-# b = list(map(int, input().split()))
-#
-# c = [0] * (max(m + b) + 1)
-# d = [0] * (max(m + b) + 1)
-#
-# a1 = 0
-# a2 = 0
-#
-# for i in range(n):
-#     if m[i] > k:
-#         continue
-#     c[m[i]] += 1
-#     a1 += d[k - m[i]]
-#
-# for i in range(n):
-#     if b[i] > k:
-#         continue
-#     d[b[i]] += 1
-#     a2 += c[k - b[i]]
-#
-# if a1 > a2:
-#     print("Mahmoud".capitalize())
-# elif a1 == a2:
-#     print("Draw".capitalize())
-# else:
-#     print("Bashar".capitalize())
+n, k = map(int, input().split())
+m = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+c = [0] * (max(m + b) + 1)
+d = [0] * (max(m + b) + 1)
+
+a1 = 0
+a2 = 0
+
+for i in range(n):
+    if m[i] > k:
+        continue
+    c[m[i]] += 1
+    a1 += d[k - m[i]]
+
+for i in range(n):
+    if b[i] > k:
+        continue
+    d[b[i]] += 1
+    a2 += c[k - b[i]]
+
+if a1 > a2:
+    print("Mahmoud".capitalize())
+elif a1 == a2:
+    print("Draw".capitalize())
+else:
+    print("Bashar".capitalize())
 
 # 7 3
 # 1 1 2 3 4 1 2
