7edf3c8133149c2d7abf5e99ba2b8b9cfaf29fdf
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-06-06 14:43:28 +0600

    React Py initial program

--- ReactPy/reactpytest.py
+++ ReactPy/reactpytest.py
@@ -1,22 +1,13 @@
-from reactpy import ReactPy, Component, render, useState, useEffect, html, run
-
-class App(Component):
-    def __init__(self):
-        super().__init__()
-        self.state = useState(0)
-
-    def render(self):
-        return html.div(
-            html.h1('Hello World!'),
-            html.p(f'You clicked {self.state} times'),
-            html.button(
-                {
-                    'onClick': lambda: self.setState(self.state + 1)
-                },
-                'Click me'
-            )
-        )
+from reactpy import component, html, run
 
 
-if __name__ == '__main__':
-    run(App)
+@component
+def App():
+    return html.div(
+        html.h1("Hello, world!"),
+        html.p("This is a paragraph."),
+        html.button("Click me!")
+    )
+
+
+run(App)
