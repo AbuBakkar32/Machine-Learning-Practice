1ef9e47808ed204d2d4e0f21f75f9d0442c3077e
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-03-17 13:55:54 +0600

    Schools-dataset

--- OOP/Bank.py
+++ OOP/Bank.py
@@ -1,23 +1,35 @@
-class BankAccount:
-    bank_name = 'ABC bank, XYZ Street, New Delhi'
+# class BankAccount:
+#     bank_name = 'ABC bank, XYZ Street, New Delhi'
+#
+#     def __init__(self, name, balance=0, bank=bank_name):
+#         self.name = name
+#         self.balance = balance
+#         self.bank = bank
+#
+#     def display(self):
+#         print(self.name, self.balance, self.bank)
+#
+#     def withdraw(self, amount):
+#         self.balance -= amount
+#
+#     def deposit(self, amount):
+#         self.balance += amount
+#
+#
+# a1 = BankAccount('Mike', 200, 'PQR Bank Delhi')
+# a2 = BankAccount('Tom')
+#
+# a1.display()
+# a2.display()
 
-    def __init__(self, name, balance=0, bank=bank_name):
-        self.name = name
-        self.balance = balance
-        self.bank = bank
 
-    def display(self):
-        print(self.name, self.balance, self.bank)
-
-    def withdraw(self, amount):
-        self.balance -= amount
-
-    def deposit(self, amount):
-        self.balance += amount
+def IsPrimeNumber(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return print(f'{num} is not a prime number')
+    return print(f'{num} is a prime number')
 
 
-a1 = BankAccount('Mike', 200, 'PQR Bank Delhi')
-a2 = BankAccount('Tom')
-
-a1.display()
-a2.display()
+IsPrimeNumber(2)
