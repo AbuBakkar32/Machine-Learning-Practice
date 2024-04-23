baf8357832f8f1a16ab0b66ef072f46b4e8f21da
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-04-23 10:27:39 +0600

    Schools-dataset

---
+++ Automation/Appium/TestCase1.py
@@ -0,0 +1,33 @@
+import unittest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    appPackage='com.android.settings',
+    appActivity='.Settings',
+    language='en',
+    locale='US'
+)
+
+appium_server_url = 'https://automationservicesbd.com/'
+
+
+class TestAppium(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_find_battery(self) -> None:
+        el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="About"]')
+        el.click()
+
+
+if __name__ == '__main__':
+    unittest.main()
