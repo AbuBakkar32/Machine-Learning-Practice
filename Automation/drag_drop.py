1eccdc666d0729ef3c49debdb1ea4013c440b6d3
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-03 20:14:57 +0600

    Schools-dataset

--- Automation/drag_drop.py
+++ Automation/drag_drop.py
@@ -1,10 +1,12 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 # Initialize a Selenium webdriver (you may need to specify the path to your webdriver)
-driver = webdriver.Chrome(executable_path)
+driver = webdriver.Chrome()
 driver.maximize_window()
-driver.implicitly_wait(3)
+# driver.implicitly_wait(3)
 
 # Navigate to the webpage where you want to perform drag and drop
 driver.get("https://jqueryui.com/resources/demos/droppable/default.html")
@@ -20,6 +22,9 @@ actions = ActionChains(driver)
 
 # Perform drag and drop operation
 actions.drag_and_drop(draggable_element, droppable_element).perform()
+
+# set time for wait
+time.sleep(3)
 
 # Close the Selenium webdriver
 driver.quit()
