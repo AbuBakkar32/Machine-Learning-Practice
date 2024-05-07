ba1438e3a317257d0a64f37eeadba908c6cb8c8d
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-07 20:40:33 +0600

    test behaviour

--- scratch/features/steps/tests.py
+++ scratch/features/steps/tests.py
@@ -24,3 +24,19 @@ def step_impl(context):
     :type context: behave.runner.Context
     """
     raise NotImplementedError(u'STEP: Then verify the logo on the page')
+
+
+@given("launch the chrome browser")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Given launch the chrome browser')
+
+
+@when("open the OrangeHRM Home Page")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: When open the OrangeHRM Home Page')
