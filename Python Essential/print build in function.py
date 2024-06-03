24884002fe52a53589d9829dff7a8f535160c37f
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-06-03 09:16:37 +0600

    test behaviour

--- Python Essential/print build in function.py
+++ Python Essential/print build in function.py
@@ -2,12 +2,12 @@
 import re
 import time
 
-# list = []
-# for j in dir(__builtins__):
-#     if not re.match(r'^[A-Z_]', j):
-#         list.append(j)
-#     for a, b, c in zip(list[::3], list[1::3], list[2::3]):
-#         print('{:<20}{:<20}{:<}'.format(a, b, c))
+list = []
+for j in dir(__builtins__):
+    if not re.match(r'^[A-Z_]', j):
+        list.append(j)
+    for a, b, c in zip(list[::3], list[1::3], list[2::3]):
+        print('{:<20}{:<20}{:<}'.format(a, b, c))
 
 
 # name = "Abubakkar"
