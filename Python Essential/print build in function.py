3c0d4ff8f14c33e2f663b157c9875b7f61f869ea
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-06-12 17:09:47 +0600

    test automation

--- Python Essential/print build in function.py
+++ Python Essential/print build in function.py
@@ -1,16 +1,16 @@
 # How to print all Built-In functions in Python?
-import re
-import time
-
-list = []
-count = 0
-for j in dir(__builtins__):
-    if not re.match(r'^[A-Z_]', j):
-        list.append(j)
-        count += 1
-
-for a, b, c in zip(list[::3], list[1::3], list[2::3]):
-    print('{:<20}{:<20}{:<}'.format(a, b, c))
+# import re
+# import time
+#
+# list = []
+# count = 0
+# for j in dir(__builtins__):
+#     if not re.match(r'^[A-Z_]', j):
+#         list.append(j)
+#         count += 1
+#
+# for a, b, c in zip(list[::3], list[1::3], list[2::3]):
+#     print('{:<20}{:<20}{:<}'.format(a, b, c))
 
 
 # name = "Abubakkar"
