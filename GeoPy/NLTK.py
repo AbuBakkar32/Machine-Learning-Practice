f91e7da81041ecbddcea47a764a8d992534be26a
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-12-08 00:11:03 +0100

    NLTK program change

--- GeoPy/NLTK.py
+++ GeoPy/NLTK.py
@@ -5,23 +5,15 @@ from string import punctuation
 from colorama import Fore
 
 # Example sentence
-count = 0
 example_sent = "This is a sample sentence, showing off the stop words filtration."
 
 # Load English stopwords
 stop_words = set(stopwords.words('english'))
-for word in stop_words:
-    count +=1
-    print(word, end=", ")
-print(f"\nTotal Stop Words Found {count}")
-
 
 # Tokenize and filter in one step
 filtered_sentence = [
-    word for word in wordpunct_tokenize(example_sent)
+    word.upper() for word in wordpunct_tokenize(example_sent)
     if word.lower() not in stop_words and word not in punctuation
 ]
 
 print(filtered_sentence)
-
-
